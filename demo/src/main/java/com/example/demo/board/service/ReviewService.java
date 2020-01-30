4aba80ead4f7a1fd4cdf00ce2d053456acafe2e5
beetee <beeeting1001@gmail.com>
2020-01-31 01:35:05 +0900

    리뷰 구현 중

---
+++ demo/src/main/java/com/example/demo/board/service/ReviewService.java
@@ -0,0 +1,39 @@
+package com.example.demo.board.service;
+
+import java.util.List;
+
+import javax.annotation.Resource;
+
+import org.springframework.stereotype.Service;
+
+import com.example.demo.board.domain.ReviewVO;
+import com.example.demo.board.mapper.ReviewMapper;
+
+@Service("com.example.demo.board.service.ReviewService")
+public class ReviewService {
+	@Resource(name="com.example.demo.board.mapper.ReviewMapper")
+	ReviewMapper reviewMapper;
+	
+	public int reviewInsert(ReviewVO reviewVO) throws Exception {
+		return reviewMapper.upload(reviewVO);
+	}
+	
+	public List<ReviewVO> readAllReview(){
+		return reviewMapper.read();
+	}
+	
+	// Update
+	public int updateReview(String id) throws Exception{
+		return reviewMapper.update(id);
+	}
+	
+	// Delete
+	public int deleteReview(String id) throws Exception{
+		return reviewMapper.delete(id);
+	}
+		
+	// Search
+	public List<ReviewVO> searchReview(String name){
+		return reviewMapper.search(name);
+	}
+}
