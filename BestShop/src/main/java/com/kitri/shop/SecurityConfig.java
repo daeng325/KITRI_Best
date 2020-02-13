9d9808cc8eb7fda573fc512d76ea4f4aad431827
yunjeong <yuj325@naver.com>
2020-02-13 23:57:05 +0900

    https & xxs filter

--- BestShop/src/main/java/com/kitri/shop/SecurityConfig.java
+++ BestShop/src/main/java/com/kitri/shop/SecurityConfig.java
@@ -117,65 +117,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
      * lucy-xss-filter
      *  
      * */
-    @Bean
-    public FilterRegistrationBean getFilterRegistrationBean(){
-        FilterRegistrationBean registrationBean = new FilterRegistrationBean();
-        registrationBean.setFilter(new XssEscapeServletFilter());
-        registrationBean.setOrder(1);
-        registrationBean.addUrlPatterns("/*");    //filter를 거칠 url patterns
-        return registrationBean;
+	@Bean
+    public FilterRegistrationBean<XssEscapeServletFilter> filterRegistrationBean() {
+        FilterRegistrationBean<XssEscapeServletFilter> filterRegistration = new FilterRegistrationBean<>();
+        filterRegistration.setFilter(new XssEscapeServletFilter());
+        filterRegistration.setOrder(1);
+        filterRegistration.addUrlPatterns("/*");
+
+        return filterRegistration;
     }
-	
-//	private Filter ssoFilter() {
-//		  CompositeFilter filter = new CompositeFilter();
-//		  List<Filter> filters = new ArrayList<>();
-//		  filters.add(ssoFilter(facebook(), "/login/facebook"));
-//		  filters.add(ssoFilter(github(), "/login/github"));
-//		  filter.setFilters(filters);
-//		  return filter;
-//		}
-//		
-//		private Filter ssoFilter(ClientResources client, String path) {
-//		  OAuth2ClientAuthenticationProcessingFilter filter = new OAuth2ClientAuthenticationProcessingFilter(path);
-//		  OAuth2RestTemplate template = new OAuth2RestTemplate(client.getClient(), oauth2ClientContext);
-//		  filter.setRestTemplate(template);
-//		  UserInfoTokenServices tokenServices = new UserInfoTokenServices(
-//		      client.getResource().getUserInfoUri(), client.getClient().getClientId());
-//		  tokenServices.setRestTemplate(template);
-//		  filter.setTokenServices(tokenServices);
-//		  return filter;
-//		}
-//		
-//		class ClientResources {
-//		  @NestedConfigurationProperty
-//		  private AuthorizationCodeResourceDetails client = new AuthorizationCodeResourceDetails();
-//		  @NestedConfigurationProperty
-//		  private ResourceServerProperties resource = new ResourceServerProperties();
-//		  public AuthorizationCodeResourceDetails getClient() {
-//		    return client;
-//		  }
-//		  public ResourceServerProperties getResource() {
-//		    return resource;
-//		  }
-//		}
-//		
-//		@Bean
-//		@ConfigurationProperties("github")
-//		public ClientResources github() {
-//		  return new ClientResources();
-//		}
-//		@Bean
-//		@ConfigurationProperties("facebook")
-//		public ClientResources facebook() {
-//		  return new ClientResources();
-//		}
-//		
-//		@Bean
-//		public FilterRegistrationBean oauth2ClientFilterRegistration(
-//		    OAuth2ClientContextFilter filter) {
-//		  FilterRegistrationBean registration = new FilterRegistrationBean();
-//		  registration.setFilter(filter);
-//		  registration.setOrder(-100);
-//		  return registration;
-//		}
 }
